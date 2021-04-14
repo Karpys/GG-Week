f4258b37d7cec29ea8e0a3e759a3fa4147a559a1
Thomas <thomas.takagi@edu.devinci.fr>
2021-04-14 15:55:04 +0200

    [Feat]TT_ShotingSystem

---
+++ Assets/Thomas/Script/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform shotPos;
+
+    private float timeBtwShots;
+    private float startTimeBtwShots;
+
+    private void Update()
+    {
+        // Weapon Rotation
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.up = direction;
+
+        if(timeBtwShots <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(projectile, shotPos.position, shotPos.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
