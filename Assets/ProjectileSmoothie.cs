fc5beae968393f7045786e5976ba0faf2a8adb1b
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-04-15 15:47:02 +0200

    [Feat/Asset] Smoothie Shot

---
+++ Assets/ProjectileSmoothie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSmoothie : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Smoothie;
+    void Start()
+    {
+        Instantiate(Smoothie, transform.position, transform.rotation);
+        transform.Rotate(0, 0, 20);
+        Instantiate(Smoothie, transform.position, transform.rotation);
+        transform.Rotate(0, 0, -40);
+        Instantiate(Smoothie, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
