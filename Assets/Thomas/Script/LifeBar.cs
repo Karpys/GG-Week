fe8b510ff565136fed4218792354d44f0505cf23
Thomas <thomas.takagi@edu.devinci.fr>
2021-04-16 10:39:39 +0200

    [Feat]TT_LifeBar

---
+++ Assets/Thomas/Script/LifeBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeBar : MonoBehaviour
+{
+    public Sprite lifeZero, lifeOne, lifeTwo, lifeTree;
+    public CharaCircle charaCircle;
+    public int currentHealth;
+
+    private void Start()
+    {
+        charaCircle = FindObjectOfType<CharaCircle>();
+    }
+
+    private void FixedUpdate()
+    {
+        currentHealth = charaCircle.currentHealth;
+        // TEST
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            --currentHealth;
+        }*/
+
+        if (currentHealth == 3)
+        {
+            GetComponent<Image>().sprite = lifeTree;
+        }
+        else if (currentHealth == 2)
+        {
+            GetComponent<Image>().sprite = lifeTwo;
+        }
+        else if (currentHealth == 1)
+        {
+            GetComponent<Image>().sprite = lifeOne;
+        }
+        else if (currentHealth == 0)
+        {
+            GetComponent<Image>().sprite = lifeZero;
+        }
+    }
+}
